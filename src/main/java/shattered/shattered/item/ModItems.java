96d55857be2d5e6d0290fc0e9af86f376e878b7d
valor <isaacisapop@gmail.com>
2024-12-10 19:41:14 -0500

    spilts the mod

--- src/main/java/shattered/shattered/item/ModItems.java
+++ src/main/java/shattered/shattered/item/ModItems.java
@@ -14,15 +14,20 @@ public class ModItems {
 
 
     //
-    public static final Item shattered_origin = registerItem("shattered_origin", new Item(new FabricItemSettings().maxCount(16)));
+    public static final Item origin_shard = registerItem("origin_shard", new Item(new FabricItemSettings().maxCount(16)));
+
+    //
+    public static final Item origins_shard = registerItem("origins_shard", new Item(new FabricItemSettings().maxCount(16)));
 
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier( Originsshattered.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, new Identifier(Originsshattered.MOD_ID, name), item);
     }
 
     public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
-        entries.add(shattered_origin);
+        entries.add(origin_shard);
+        entries.add(origins_shard);
+
     }
 
 
